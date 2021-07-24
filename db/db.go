552cd29f24aed4ad1d8dbe68949909ea5cdccffe
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-07-24 23:15:38 +0100

    updates connection vars
    Signed-off-by: Jubril Oyetunji <cyberdev01@protonmail.com>

--- db/db.go
+++ db/db.go
@@ -12,15 +12,16 @@ var DB *redis.Client
 var ctx = context.Background()
 
 var (
-	HOST = os.Getenv("REDIS_HOST")
-	PORT = os.Getenv("REDIS_PORT")
+	HOST     = os.Getenv("REDIS_CACHE_HOST")
+	PORT     = os.Getenv("REDIS_PORT")
+	PASSWORD = os.Getenv("REDIS_CACHE_AUTH_TOKEN")
 )
 
 // connect creates a connection to redis
 func Connect() {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     HOST + ":" + PORT,
-		Password: "",
+		Password: PASSWORD,
 		DB:       0,
 	})
 	err := rdb.Ping(ctx).Err()
